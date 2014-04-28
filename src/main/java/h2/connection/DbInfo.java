864f065ea1331dac710dd456c94619eccfa64241
mcmoe <mcxmoe@gmail.com>
2014-04-28 20:58:53 +0200

    Change DB name from test to scrapa

--- src/main/java/h2/connection/DbInfo.java
+++ src/main/java/h2/connection/DbInfo.java
@@ -5,8 +5,8 @@ package h2.connection;
  * Created by mcmoe on 4/25/2014.
  */
 public class DbInfo {
-    static final String H2_MEM_DB = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
-    static final String H2_EMBD_DB = "jdbc:h2:./test;DB_CLOSE_DELAY=-1";
+    static final String H2_MEM_DB = "jdbc:h2:mem:scrapa;DB_CLOSE_DELAY=-1";
+    static final String H2_EMBD_DB = "jdbc:h2:./scrapa;DB_CLOSE_DELAY=-1";
     static final String PASSWORD = "password";
     static final String USER = "user";
 }
